11d243cd062dcfa14c2ac0deda45fdb7be5ce0ff
Oswaldo Luis Sierra Bran <43079070+oswaldosierra@users.noreply.github.com>
2019-11-30 21:18:04 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,49 @@
+var txt = document.getElementById("texto")
+document.addEventListener("keyup", eventoteclado)
+var resul = document.getElementById("resul")
+var c = 0
+var ids = 1 //variable de id de span de resul
+var re //resultado escrito
+txt.focus()
+function focust(){
+    txt.focus()
+}
+function insert(num){
+    txt.value = txt.value + num
+}
+// funcion de borrado
+function limpiar(){
+    if (c == 1) {
+        resul.innerHTML = resul.innerHTML + "<br>" + "<span id=" + ids + ">" + re + " = " + txt.value
+    }
+    txt.value = "";
+    c = 0
+}
+// boton borrar 
+function back(){
+    txt.value = txt.value.substring(0,txt.value.length-1)
+}
+// tecla
+function eventoteclado (teclado){
+    if(teclado.keyCode == 13){
+        equal()
+    }
+    if(teclado.keyCode == 46){ 
+        limpiar()
+    }
+}
+// boton calcular
+function equal(){
+    var total = eval(txt.value)
+    if (total != undefined && total != txt.value){
+        re = txt.value
+        txt.value = total
+        c = 1
+    }
+}
+// toda la pantalla
+// var body = document.getElementById("body")
+// var header = document.getElementById("resul")
+// var bg = document.getElementById("bg")
+// body.style.minHeight = (body.offsetHeight - header.offsetHeight) + "px"
+// bg.style.minHeight = (bg.offsetHeight - header.offsetHeight) + "px"
